d004c1f23bb7bc601c0e508451e71a2b14befe54
the-sneg <the.sneg.sasha@gmail.com>
2022-05-26 19:52:10 +0300

    Update task-01.js

---
+++ js/task-01.js
@@ -0,0 +1,13 @@
+// const categoriesListRef = document.querySelector('#categories');
+// console.log('Number of categories: ', categoriesListRef.children.length);
+
+// const categoriesListTitleRef = document.querySelectorAll('.item h2');
+
+// const categoriesListNameRef = document.querySelectorAll('.item ul')
+
+// for (let i = 0; i < categoriesListTitleRef.length; i += 1) {
+
+//     console.log(`Category: ${categoriesListTitleRef[i].textContent}`);
+//     console.log(`Elements: ${categoriesListNameRef[i].children.length}`)
+
+// };
