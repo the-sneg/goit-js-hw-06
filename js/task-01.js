d6a601e88ec9520adaaf308e8e2ea0ae9bc5b220
the-sneg <the.sneg.sasha@gmail.com>
2022-05-27 22:59:09 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,11 +1,8 @@
 const categoriesListRef = document.querySelector("#categories");
 console.log("Number of categories: ", categoriesListRef.children.length);
 
-const categoriesListTitleRef = document.querySelectorAll(".item h2");
-
-const categoriesListNameRef = document.querySelectorAll(".item ul");
-
-for (let i = 0; i < categoriesListTitleRef.length; i += 1) {
-  console.log(`Category: ${categoriesListTitleRef[i].textContent}`);
-  console.log(`Elements: ${categoriesListNameRef[i].children.length}`);
-}
+const allLiItem = document.querySelectorAll(".item");
+allLiItem.forEach((item) => {
+  console.log(`Category: ${item.firstElementChild.textContent}`);
+  console.log(`Elements: ${item.lastElementChild.children.length}`);
+});
